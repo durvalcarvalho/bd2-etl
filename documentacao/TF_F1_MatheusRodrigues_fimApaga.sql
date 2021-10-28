e321fdf12c5e0537cc303bd162ad5866cf5224c4
Durval Carvalho <durvalcsouza@outlook.com>
2021-10-27 22:56:14 -0300

    Update files

---
+++ documentacao/TF_F1_MatheusRodrigues_fimApaga.sql
@@ -0,0 +1,20 @@
+-- 	-------- Trabalho Final - Tema F1 - ETL - ENEM ----------
+--
+-- SCRIPT DE APAGA (DDL)
+--
+-- Data Criacao ...........: 25/10/2021
+-- Autor(es) ..............: Durval Carvalho e Matheus Gabriel
+-- Banco de Dados .........: MySQL 8
+-- Base de Dados (nome) ...: TF_Apr_F_MatheusRodrigues
+--
+-- PROJETO => 01 Base de Dados
+--         => 04 Tabelas
+--
+-- ---------------------------------------------------------
+
+USE TF_Apr_F_MatheusRodrigues;
+
+DROP TABLE realiza;
+DROP TABLE PROVA;
+DROP TABLE CANDIDATO;
+DROP TABLE MUNICIPIO;
